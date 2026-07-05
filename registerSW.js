e34209efd0a7ac2d71023983a5ad7608ce751906
Namatashabira <namatashabira@gmail.com>
2026-07-05 23:23:21 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/EducFarm/sw.js', { scope: '/EducFarm/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
